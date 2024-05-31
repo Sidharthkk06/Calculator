e4cdd62cc18e2bf6d8b0631e0489d314b2fc9603
Sidharthkk06 <sidharthkishor.k@gmail.com>
2024-05-31 20:36:40 +0530

    Initial commit

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ function backspace() {
     clear = final_result.value.slice(0, -1)
     final_result.value = clear;
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const themeToggle = document.getElementById('themeToggle');
+    themeToggle.addEventListener('change', function() {
+        document.body.classList.toggle('light-theme');
+        document.body.classList.toggle('dark-theme');
+    });
+});
